e0109a9a8cc5104bbded9f7d420976553e2ce6a9
Zichun (Jerry) Gao <zichungao88@gmail.com>
2023-09-30 23:20:15 -0500

    Copium thru case study

--- cluster_covid_data.m
+++ cluster_covid_data.m
@@ -1,7 +1,10 @@
 load('COVIDbyCounty.mat');
+CNTY_CENSUS_SORTED = sortrows(CNTY_CENSUS, 3);
 
 training = []; % 75% of all data
 testing = []; % 25% of all data
+trainingCases = [];
+testingCases = [];
 
 for i = 1:(length(CNTY_CENSUS_SORTED.fips)) % 3-1 ratio
     if mod(i, 4) == 0
@@ -11,6 +14,16 @@ for i = 1:(length(CNTY_CENSUS_SORTED.fips)) % 3-1 ratio
     end
 end
 
+for i = 1:height(training) % index from CNTY_COVID (original raw data)
+    trainingCases = [trainingCases; CNTY_COVID(CNTY_CENSUS.fips == ...
+        training(i, :).fips, :)];
+end
+
+for i = 1:height(testing)
+    testingCases = [testingCases; CNTY_COVID(CNTY_CENSUS.fips == ...
+        testing(i, :).fips, :)];
+end
+
 k = 9; % subject to change
-[labels, centroids] = kmeans(CNTY_COVID, k);
-CNTY_CENSUS_SORTED = sortrows(CNTY_CENSUS, 3);
+% apply k-means algorithm on training group
+[labels, centroids] = kmeans(trainingCases, k);
