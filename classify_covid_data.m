cf1d366b5dcdb5ad7beeed929ffe79d49dada77c
Zichun (Jerry) Gao <zichungao88@gmail.com>
2023-10-01 19:40:46 -0500

    Copium thru el goofy ahh case study

--- classify_covid_data.m
+++ classify_covid_data.m
@@ -1,11 +1,37 @@
 nearestNeighbor = [];
-centroidAssignment = [];
+centroidTesting = [];
 
-for i = 1:width(centroids)
-    for j = 1:height(centroids)
-        nearestNeighbor = [nearestNeighbor; norm(testingCases(i, :) - ...
-            centroids(j, :))];
+for i = 1:height(centroids)
+    for j = 1:height(testingCases)
+        nearestNeighbor = [nearestNeighbor, norm(centroids(i, :) - ...
+            testingCases(j, :))]; % row
     end
-    centroidAssignment = [centroidAssignment; min(nearestNeighbor)];
-    nearestNeighbor = [];
+    % append each row to matrix
+    centroidTesting = [centroidTesting; nearestNeighbor];
+    nearestNeighbor = []; % clear current row for the next row of entries
 end
+
+correct = 0;
+incorrect = 0;
+% set initial minimum value for later comparison
+minimum = centroidTesting(1, 1);
+index = 0;
+
+for i = 1:width(centroidTesting) % loop through matrix of centroids
+    for j = 1:height(centroidTesting)
+        if centroidTesting(j, i) < minimum % find minimum value
+            minimum = centroidTesting(j, i);
+            index = j; % find index at minimum value
+        end
+    end
+    % check if testing data matches centroids from training data
+    if centroidTraining(index) == testing.DIVISION(i)
+        correct = correct + 1;
+    else
+        incorrect = incorrect + 1;
+    end
+end
+
+accuracy = correct / (correct + incorrect);
+
+disp("The accuracy is " + round(accuracy * 100 * 100) / 100 + "%");
